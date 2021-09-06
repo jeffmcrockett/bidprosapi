e7f1aadeb9c499755c4017da126995d3c4eb2722
Jeff Crockett <jeffcrockett@jeffs-imac.lan>
2021-09-05 20:31:20 -0500

    index updated

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ require('dotenv').config();
 // set up our express app
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+    });
+
 // connect to mongodb
 
 let db = process.env.MONGOURI;
