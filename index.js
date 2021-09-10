c3a66c4de4b2f9c3988e92906086bd2379cf52da
Jeff Crockett <jeffcrockett@jeffs-imac.lan>
2021-09-09 21:39:06 -0500

    update

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
     });
