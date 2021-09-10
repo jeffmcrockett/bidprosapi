5cef14ac77f1ec884d57aa6c3bf34d620c6bfaf8
Jeff Crockett <jeffcrockett@jeffs-imac.lan>
2021-09-09 22:14:25 -0500

    update

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ const app = express();
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Accept', 'application/json');
-
-    next();
+    // next();
     });
 
 // const cors = require('cors');
